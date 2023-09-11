969579af607da922aae3b6c08b0a150f85b9ca98
fiv <1647715359@qq.com>
2023-09-11 21:51:23 +0800

    0

---
+++ THREAD/pthread.c
@@ -0,0 +1,12 @@
+//
+// Created by CXQ on 2023/9/11.
+//
+
+#include "stdio.h"
+#include "pthread.h"
+
+int main() {
+
+    pthread_t tid;
+
+}
---
+++ cmake-build-debug/CMakeFiles/LinuxCC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LinuxCC.dir/main.c.o"
+  "CMakeFiles/LinuxCC.dir/main.c.o.d"
+  "LinuxCC"
+  "LinuxCC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LinuxCC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
